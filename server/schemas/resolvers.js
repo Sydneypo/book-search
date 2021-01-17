c1526227c608b474aa3bad848f93ea9d9efaa8c4
Murphy <sydneyporter74@yahoo.com>
2021-01-16 20:01:50 -0500

    added resolver functions

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,5 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { Book, User } = require('../models');
-const { signToken } = require('../utils/auth');
+const { User } = require('../models');
 
 
 const resolvers = {
@@ -16,7 +15,7 @@ const resolvers = {
 
             throw new AuthenticationError('Not logged in');
         },
-        
+
         // get a user by username
         user: async(parent, { username }) => {
             return User.findOne({ username })
@@ -26,7 +25,37 @@ const resolvers = {
         users: async () => {
             return User.find()
                 .select('-__v -password')
-        }
+        },
+
+    },
+
+    Mutation: {
+
+        saveBook: async ({ user, body }, res) => {
+            try {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: user._id },
+                    { $addToSet: { savedBooks: body }},
+                    { new: true, runValidators: true }
+                );
+                return res.json(updatedUser);
+            } catch (err) {
+                console.log(err);
+                return res.status(400).json(err);
+            }
+        },
+
+        removeBook: async ({ user, params }, res) =>{
+            const updatedUser = await User.findOneAndUpdate(
+                { _id: user._id },
+                {$pull: { savedBooks: { bookId: params.bookId }}},
+                { new: true }
+            );
+            if (!updatedUser) {
+                return res.status(404).json({ message: "Couldn't find user with this id!"});
+            }
+            return res.json(updatedUser);
+        },
     }
 };
 
